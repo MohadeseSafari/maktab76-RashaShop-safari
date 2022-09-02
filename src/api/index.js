45d5a3cd2b7247da6ac802971b62b220622d32c4
Mahdi Khavari <mkhavari0120@gmail.com>
2022-09-02 23:07:55 +0430

    imagePush fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,93 +1,97 @@
-import axios from 'api/http';
+import axios from "api/http";
 
 // Products
 export const loadProductsApi = async ({ currentPage, limitPages }) => {
-    try {
-        const response = await axios.get(`/products?_page=${currentPage}&_limit=${limitPages}`);
-        const count = Number(response.headers["x-total-count"]);
-        return { data: response.data, count: count };
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.get(
+      `/products?_page=${currentPage}&_limit=${limitPages}`
+    );
+    const count = Number(response.headers["x-total-count"]);
+    return { data: response.data, count: count };
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //Orders
 export const loadOrdersApi = async ({ delivered, currentPage, limitPages }) => {
-    try {
-        const response = await axios.get(`/orders?delivered${delivered}&_page=${currentPage}&_limit=${limitPages}`);
+  try {
+    const response = await axios.get(
+      `/orders?delivered${delivered}&_page=${currentPage}&_limit=${limitPages}`
+    );
 
-        const count = Number(response.headers["x-total-count"]);
-        return { data: response.data, count: count };
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+    const count = Number(response.headers["x-total-count"]);
+    return { data: response.data, count: count };
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //Category
 export const loadCategoriesApi = async () => {
-    try {
-        const response = await axios.get('/category');
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.get("/category");
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //Delete Product
 export const deleteProductApi = async (id) => {
-    try {
-        const response = await axios.delete(`/products/${id}`);
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.delete(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //Update Product
 export const updateProductApi = async (product) => {
-    try {
-        const response = await axios.put(`/products/${product.id}`, product);
-        console.log(response.data)
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.put(`/products/${product.id}`, product);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //Create Product
 export const createProductApi = async (product) => {
-    try {
-        const response = await axios.post('/products', product);
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.post("/products", product);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 //upload Image
 export const uploadImageApi = async (image) => {
-    try {
-        const response = await axios.post('/upload', image);
-        return response.data.filename;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.post("/upload", image);
+    return response.data.filename;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 export const deleteImageApi = async (code) => {
-    try {
-        const response = await axios.put(`/products?image=${code}`);
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    const response = await axios.put(`/products?image=${code}`);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-export const updateOrdersApi = async (id ) => {
-    try {
-        const response = await axios.patch(`/orders/${id}`, { delivered: "true" });
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+export const updateOrdersApi = async (id) => {
+  try {
+    const response = await axios.patch(`/orders/${id}`, { delivered: "true" });
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
--- src/redux/feature/products/ProductsSlice.js
+++ src/redux/feature/products/ProductsSlice.js
@@ -1,103 +1,119 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { loadProductsApi, deleteProductApi, updateProductApi, createProductApi } from 'api/index';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  loadProductsApi,
+  deleteProductApi,
+  updateProductApi,
+  createProductApi,
+} from "api/index";
 
 const initialState = {
-    products: [],
-    loading: false,
-    error: '',
-    currentPage: 1,
-    totalCount: 0
-}
+  products: [],
+  loading: false,
+  error: "",
+  currentPage: 1,
+  totalCount: 0,
+};
 //Get Products
-export const fetchProducts = createAsyncThunk('products/fetchProducts', loadProductsApi);
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  loadProductsApi
+);
 
 //Delete Product
-export const deleteProduct = createAsyncThunk('products/deleteProducts', async (id) => {
-    await deleteProductApi(id)
-    return { id }
-});
+export const deleteProduct = createAsyncThunk(
+  "products/deleteProducts",
+  async (id) => {
+    await deleteProductApi(id);
+    return { id };
+  }
+);
 
 //Update Product
-export const updateProduct = createAsyncThunk("products/updateProducts", async(product) => {
-    return await updateProductApi(product)
-});
+export const updateProduct = createAsyncThunk(
+  "products/updateProducts",
+  async (product) => {
+    fetchProducts();
+    return await updateProductApi(product);
+  }
+);
 
 //Create Product
-export const createProduct = createAsyncThunk("products/createProducts", async (product) => {
-    return await createProductApi(product)
-});
-
-
+export const createProduct = createAsyncThunk(
+  "products/createProducts",
+  async (product) => {
+    return await createProductApi(product);
+  }
+);
 
 export const ProductsSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducer: {
+  name: "products",
+  initialState,
+  reducer: {},
+  extraReducers: (builder) => {
+    //get Products
+    builder.addCase(fetchProducts.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data;
+      state.totalCount = action.payload.count;
+      state.currentPage = action.meta.arg.currentPage;
+    });
 
-    },
-    extraReducers: (builder) => {
-        //get Products
-        builder.addCase(fetchProducts.pending, (state, action) => {
-            state.loading = true;
-        });
-        builder.addCase(fetchProducts.fulfilled, (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data;
-            state.totalCount = action.payload.count;
-            state.currentPage = action.meta.arg.currentPage;
-        });
+    builder.addCase(fetchProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
 
-        builder.addCase(fetchProducts.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        });
+    //delete Product
+    builder.addCase(deleteProduct.pending, (state, action) => {
+      state.loading = true;
+    });
 
-        //delete Product
-        builder.addCase(deleteProduct.pending, (state, action) => {
-            state.loading = true;
-        });
+    builder.addCase(deleteProduct.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = state.products.filter(
+        (product) => +product.id !== action.payload.id
+      );
+    });
 
-        builder.addCase(deleteProduct.fulfilled, (state, action) => {
-            state.loading = false;
-            state.products = state.products.filter((product) => +product.id !== action.payload.id);
-        })
+    builder.addCase(deleteProduct.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
 
-        builder.addCase(deleteProduct.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        });
+    //update Product
+    builder.addCase(updateProduct.pending, (state, action) => {
+      state.loading = true;
+    });
 
-        //update Product
-        builder.addCase(updateProduct.pending, (state, action) => {
-            state.loading = true;
-        });
+    builder.addCase(updateProduct.fulfilled, (state, action) => {
+      console.log(action);
+      state.loading = false;
+      state.products = action.payload.data;
+    });
 
-        builder.addCase(updateProduct.fulfilled, (state, action) => {
-            console.log(action)
-            state.loading = false;
-            state.products = action.payload.data;
-        })
+    builder.addCase(updateProduct.rejected, (state, action) => {
+      console.log(action);
+      state.loading = false;
+      state.error = action.payload;
+    });
 
-        builder.addCase(updateProduct.rejected, (state, action) => {
-            console.log(action)
-            state.loading = false;
-            state.error = action.payload;
-        });
+    //create Product
+    builder.addCase(createProduct.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(createProduct.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products.push(action.payload);
+    });
 
-        //create Product
-        builder.addCase(createProduct.pending, (state, action) => {
-            state.loading = true;
-        });
-        builder.addCase(createProduct.fulfilled, (state, action) => {
-            state.loading = false;
-            state.products.push(action.payload);
-        });
-
-        builder.addCase(createProduct.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        });
-    }
-})
+    builder.addCase(createProduct.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+  },
+});
 
 export default ProductsSlice.reducer;
